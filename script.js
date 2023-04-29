c78798b70e44bbb70de848e74715c869201892eb
Timokinator <timoke@web.de>
2023-04-29 23:09:07 +0200

    quiz-card ready

---
+++ script.js
@@ -0,0 +1,61 @@
+let questions = [
+    {
+        "question": "Wer ist der pseudonyme Erfinder von Bitcoin?",
+        "answer_1": "Meister Eder",
+        "answer_2": "Craig Wright",
+        "answer_3": "Satoshi Nakamoto",
+        "answer_4": "Hal Finney",
+        "right_answer": 3,
+    },
+    {
+        "question": "Was ist die maximale Anzahl an Bitcoins, die jemals im Umlauf sein werden?",
+        "answer_1": "21 Millionen",
+        "answer_2": "100 Millionen",
+        "answer_3": "50 Millionen",
+        "answer_4": "1 Million",
+        "right_answer": 1,
+    },
+    {
+        "question": "Wie wird der Prozess genannt, bei dem neue Bitcoins durch die Lösung komplexer mathematischer Probleme geschaffen werden?",
+        "answer_1": "Farming",
+        "answer_2": "Mining",
+        "answer_3": "Digging",
+        "answer_4": "Harvesting",
+        "right_answer": 2,
+    },
+    {
+        "question": "Was ist eine Blockchain?",
+        "answer_1": "Eine dezentrale Datenbank",
+        "answer_2": "Ein Online-Shop für Bitcoin",
+        "answer_3": "Eine Hardware-Wallet",
+        "answer_4": "Eine Mining-Software",
+        "right_answer": 1,
+    },
+    {
+        "question": "Was ist ein 'Halving' in Bezug auf Bitcoin?",
+        "answer_1": "Eine neue Kryptowährung",
+        "answer_2": " Eine Art von Wallet",
+        "answer_3": "Ein spezieller Mining-Algorithmus",
+        "answer_4": "Eine Halbierung der Belohnung für das Mining",
+        "right_answer": 4,
+    },
+    {
+        "question": "Wie wird der Konsens-Algorithmus genannt, der Bitcoin zugrunde liegt?",
+        "answer_1": "Proof of Stake",
+        "answer_2": "Proof of Concept",
+        "answer_3": "Proof of Work",
+        "answer_4": "Proof of Burn",
+        "right_answer": 3,
+    },
+    {
+        "question": "Was ist ein Fork in Bezug auf Bitcoin?",
+        "answer_1": "Eine Belohnung für das Mining",
+        "answer_2": "Eine Abspaltung der Blockchain in zwei separate Versionen",
+        "answer_3": "Eine Art von Wallet-Software",
+        "answer_4": "Eine Überweisung von Bitcoin-Guthaben",
+        "right_answer": 2,
+    },
+];
+
+
+
