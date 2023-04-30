8d068ab6f89b807bc910c903bc135fc9ba76206c
Timokinator <timoke@web.de>
2023-04-30 23:38:19 +0200

    nearly finished

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let questions = [
         "right_answer": 3,
     },
     {
-        "question": "Was ist die maximale Anzahl an Bitcoins, die jemals im Umlauf sein werden?",
+        "question": "Was ist die maximale Anzahl an Bitcoin, die jemals im Umlauf sein werden?",
         "answer_1": "21 Millionen",
         "answer_2": "100 Millionen",
         "answer_3": "50 Millionen",
@@ -16,7 +16,7 @@ let questions = [
         "right_answer": 1,
     },
     {
-        "question": "Wie wird der Prozess genannt, bei dem neue Bitcoins durch die Lösung komplexer mathematischer Probleme geschaffen werden?",
+        "question": "Wie wird der Prozess genannt, bei dem neue Bitcoin durch die Lösung komplexer mathematischer Probleme geschaffen werden?",
         "answer_1": "Farming",
         "answer_2": "Mining",
         "answer_3": "Digging",
@@ -58,4 +58,62 @@ let questions = [
 ];
 
 
+let currentQuestion = 0;
 
+
+function init() {
+    document.getElementById("all-questions").innerHTML = questions.length;
+
+    showQuestion();
+}
+
+
+function showQuestion() {
+
+    if (currentQuestion >= questions.length) {
+        document.getElementById('endScreen').classList.toggle('d-none');
+        document.getElementById('questionBody').classList.toggle('d-none');
+    } else {
+        let question = questions[currentQuestion];
+
+        document.getElementById('actual-question').innerHTML = currentQuestion + 1;
+        document.getElementById("question-text").innerHTML = question["question"];
+        document.getElementById("answer_1").innerHTML = question["answer_1"];
+        document.getElementById("answer_2").innerHTML = question["answer_2"];
+        document.getElementById("answer_3").innerHTML = question["answer_3"];
+        document.getElementById("answer_4").innerHTML = question["answer_4"];
+    };
+};
+
+
+function answer(selection) {
+    let correctAnswer = questions[currentQuestion]['right_answer'];
+    let selectedQuestionNumner = selection.slice(-1);
+    let idOfRightAnswer = `answer_${correctAnswer}`;
+
+    if (correctAnswer == selectedQuestionNumner) {
+        document.getElementById(selection).parentNode.classList.add('bg-success');
+    } else {
+        document.getElementById(selection).parentNode.classList.add('bg-danger');
+        document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
+    };
+    document.getElementById('next-button').disabled = false;
+
+}
+
+
+function nextQuestion() {
+    currentQuestion++;
+    document.getElementById('next-button').disabled = true;
+    showQuestion();
+
+    for (let i = 1; i < 5; i++) {
+        const element = `answer_${i}`;
+        document.getElementById(element).parentElement.classList.remove('bg-success');
+        document.getElementById(element).parentNode.classList.remove('bg-danger');
+    };
+
+
+
+
+}
